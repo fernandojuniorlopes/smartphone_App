ee53f7519b36814e861007319590923fde91b4f3
Fernando Lopes <32465227+fernandojuniorlopes@users.noreply.github.com>
2019-11-27 14:44:47 +0000

    Configurada a conecçao do moongose(corrigido)

--- app.js
+++ app.js
@@ -8,6 +8,15 @@ const app = express();
 
 app.use('/smartphones', product);
 
+// Set up mongoose connection
+const mongoose = require('mongoose');
+let dev_db_url = 'mongodb+srv://someuser:abcd1234@productstutorial-ovctk.mongodb.net/test?retryWrites=true&w=majority';
+let mongoDB = process.env.MONGODB_URI || dev_db_url;
+mongoose.connect(mongoDB);
+mongoose.Promise = global.Promise;
+let db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
 let port = 1234;
 
 app.listen(port, () => {
--- controllers/smartphone.controller.js
+++ controllers/smartphone.controller.js
@@ -4,12 +4,3 @@ const Smartphone = require('../models/smartphone.model');
 exports.test = function (req, res) {
     res.send('Greetings from the Test controller!');
 };
-
-// Set up mongoose connection
-const mongoose = require('mongoose');
-let dev_db_url = 'mongodb+srv://someuser:abcd1234@productstutorial-ovctk.mongodb.net/test?retryWrites=true&w=majority';
-let mongoDB = process.env.MONGODB_URI || dev_db_url;
-mongoose.connect(mongoDB);
-mongoose.Promise = global.Promise;
-let db = mongoose.connection;
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
